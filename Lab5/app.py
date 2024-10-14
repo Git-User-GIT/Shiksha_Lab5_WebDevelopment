53fea4828f3d3728836d769494f170bc78a56993
Shiksha <shiksha1694@gmail.com>
2024-10-14 20:18:01 +0530

    updated changes

--- Lab5/app.py
+++ Lab5/app.py
@@ -19,7 +19,7 @@ def home():
     data4 = request.form['d']
     arr =np.array([[data1, data2, data3, data4]])
     pred = model.predict(arr)
-    return render_template('after.html', data-pred)
+    return render_template('after.html', data = pred)
 
 if __name__ =='__main__':
     app.run(debug=True)
